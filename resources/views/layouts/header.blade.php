2cd597251a7dff432a0dc21ba9b026e394d22471
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-24 20:15:54 +0100

    add api link in menu and connected it to api view page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,7 @@
                     <a href="{{ route($item['name']) }}" class="{{ Route::currentRouteName() === $item['name'] ? 'active' : '' }}">{{ $item['name'] }}</a>
                     @endforeach
                     <a href="{{ route('articles.index') }}" class="{{ Route::currentRouteName() ===  'articles.index' ? 'active' : '' }}">Admin</a>
+                    <a href="{{ route('articles_api') }}" class="{{ Route::currentRouteName() ===  'articles_api' ? 'active' : '' }}">Api</a>
                 </li>
             </ul>
         </nav>   
