809f177e5fd5676089c0c202ad970ee9c474d6ba
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-24 20:11:50 +0100

    add style to the project

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
                     @foreach ($menu_link as $item)
                     <a href="{{ route($item['name']) }}" class="{{ Route::currentRouteName() === $item['name'] ? 'active' : '' }}">{{ $item['name'] }}</a>
                     @endforeach
-                    <a href="{{ route('articles.index') }}">Admin</a>
+                    <a href="{{ route('articles.index') }}" class="{{ Route::currentRouteName() ===  'articles.index' ? 'active' : '' }}">Admin</a>
                 </li>
             </ul>
         </nav>   
