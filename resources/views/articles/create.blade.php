6b7392a51ab16256cf06c0bf2315d663001439e2
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-27 22:50:21 +0100

    add style to create and edit pages of articleAd

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,32 +4,37 @@ New Article
 @endsection
 @section('main_content')
         <h1>Write a New Article</h1>
-        <form action="{{ route('articles.store')}}" method="post">
+        <form class="form_container" action="{{ route('articles.store')}}" method="post">
             @csrf
-            <label for="title">Title</label>
-            <input id="input_title" type="text" name="title" placeholder="Write here the title of your post">
-            <label for="author">Author</label>
-            <input id="input_author" type="text" name="author" placeholder="Write here the author of your post">
-            <label for="body">Body</label>
-            <textarea cols="30" rows="10" id="input_body" type="text" name="body" placeholder="Write here the body of your post"></textarea>
-
-            <div>
-                <label for="category_id">Categories</label>
-                <select name="category_id" id="category_id" multiple>
-                    @foreach ($categories as $cat)
-                        <option value="{{ $cat -> id }}">{{ $cat -> title }}</option>    
-                    @endforeach
-                </select>
+            <div class="draw_container">
+                <label for="title">Title</label>
+                <input id="input_title" type="text" name="title" placeholder="Write here the title of your post">
+                <label for="author">Author</label>
+                <input id="input_author" type="text" name="author" placeholder="Write here the author of your post">
+                <label for="body">Body</label>
+                <textarea cols="30" rows="10" id="input_body" type="text" name="body" placeholder="Write here the body of your post"></textarea>
             </div>
+            <aside>
+                <div>
+                    <label for="category_id">Categories</label>
+                    <select name="category_id" id="category_id" multiple>
+                        @foreach ($categories as $cat)
+                            <option value="{{ $cat -> id }}">{{ $cat -> title }}</option>    
+                        @endforeach
+                    </select>
+                </div>
 
-            <div>
-                <label for="tags">Tags</label>
-                <select name="tags" id="tags" multiple>
-                    @foreach ($tags as $tag)
-                        <option value="{{ $tag -> id }}">{{ $tag -> title }}</option>    
-                    @endforeach
-                </select>
-            </div>
-            <button id="create_btn" type="submit">Publish</button>
+                <div>
+                    <label for="tags">Tags</label>
+                    <select name="tags" id="tags" multiple>
+                        @foreach ($tags as $tag)
+                            <option value="{{ $tag -> id }}">{{ $tag -> title }}</option>    
+                        @endforeach
+                    </select>
+                </div>
+                <div>
+                    <button id="create_btn" type="submit">Publish</button>
+                </div>
+            </aside>
         </form>
 @endsection
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -4,31 +4,35 @@ Edit Article
 @endsection
 @section('main_content')
 <h1>Edit {{ $article -> title }}</h1>
-        <form action="{{ route('articles.update', ['article'=> $article -> id])}}" method="post">
+        <form class="form_container" action="{{ route('articles.update', ['article'=> $article -> id])}}" method="post">
             @csrf
             @method('Put')
-            <label for="title">Title</label>
-            <input id="input_title" type="text" name="title" value="{{ $article -> title }}">
-            <label for="body">Body</label>
-            <textarea cols="30" rows="10" id="input_body" type="text" name="body">{{ $article -> body }}</textarea>
-            <div>
-                <label for="category_id">Categories</label>
-                <select name="category_id" id="category_id" multiple>
-                    @foreach ($categories as $cat)
-                        <option value="{{ $cat -> id }}">{{ $cat -> title }}</option>    
-                    @endforeach
-                </select>
+            <div class="draw_container">
+                <label for="title">Title</label>
+                <input id="input_title" type="text" name="title" value="{{ $article -> title }}">
+                <label for="body">Body</label>
+                <textarea cols="30" rows="10" id="input_body" type="text" name="body">{{ $article -> body }}</textarea>
             </div>
-
-            <div>
-                <label for="tags">Tags</label>
-                <select name="tags" id="tags" multiple>
-                    @foreach ($tags as $tag)
-                        <option value="{{ $tag -> id }}">{{ $tag -> title }}</option>    
-                    @endforeach
-                </select>
-            </div>
-
-            <button id="create_btn" type="submit">Edit</button>
+            <aside>
+                <div>
+                    <label for="category_id">Categories</label>
+                    <select name="category_id" id="category_id" multiple>
+                        @foreach ($categories as $cat)
+                            <option value="{{ $cat -> id }}">{{ $cat -> title }}</option>    
+                        @endforeach
+                    </select>
+                </div>
+                <div>
+                    <label for="tags">Tags</label>
+                    <select name="tags" id="tags" multiple>
+                        @foreach ($tags as $tag)
+                            <option value="{{ $tag -> id }}">{{ $tag -> title }}</option>    
+                        @endforeach
+                    </select>
+                </div>
+                <div>
+                    <button id="create_btn" type="submit">Edit</button>
+                </div>
+            </aside>
         </form>
 @endsection
